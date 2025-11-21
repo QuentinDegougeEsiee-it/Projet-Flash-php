db458992d5711c7cdb6136176393716ab22424a8
QuentinDegougeEsiee-it <quentin.degouge@edu.esiee-it.fr>
2025-11-21 09:40:34 +0100

    Update index.php

--- projet/index.php
+++ projet/index.php
@@ -8,7 +8,7 @@ $pdo = connectToDBandGetPDOdb();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Acceuil</title>
+    <title>Accueil</title>
    
     <link rel="stylesheet" href="../asset/css/index.css">
     <link rel="shortcut icon" href="../asset/images/favicon.png" type="image/x-icon">
@@ -231,3 +231,4 @@ if ($success3) {
 <?php include './partials/footer.php'; ?>
 </body>
 </html>
+
